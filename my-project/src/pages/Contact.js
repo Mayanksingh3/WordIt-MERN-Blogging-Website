4eb76f22869c029d64d6c9a5659e6b42dc2f9233
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2021-04-24 17:28:23 +0530

    Updated Contact UI

--- my-project/src/pages/Contact.js
+++ my-project/src/pages/Contact.js
@@ -1,4 +1,9 @@
 import React from "react";
+import me from "../assets/mayank.jpg";
+import "../css/Contact.css";
+import { GoMail, GoMarkGithub } from "react-icons/go";
+import { Link } from "react-router-dom";
+import { GrLinkedin } from "react-icons/gr";
 
 export default function Contact() {
   return (
@@ -6,10 +11,60 @@ export default function Contact() {
       <div className="card p-3 main-body">
         <div className="banner bg-warning">
           <h1>Hello, Welcome to our Contact Page!</h1>
+          <div>
+            <h3>Developer : </h3>
+          </div>
         </div>
-        <div>
-          <img src="../assets/login.png" alt="" />
-          <h4>ehej</h4>
+        <div className="d-flex  mt-3">
+          <img className="mr-3" src={me} alt="Developer" />
+          <div className="">
+            <div className="d-flex container-fluid justify-content-center">
+              <h3>Mayank Kumar Singh</h3>
+            </div>
+            <br />
+            <br />
+            <h4>Contact Me :</h4>
+            <a
+              className="text-dark"
+              target="_blank"
+              href="mailto:mayank.singh1_cs18@gla.ac.in"
+            >
+              <h5>{<GoMail />} : mayank.singh1_cs18@gla.ac.in</h5>
+            </a>
+            <a
+              className="text-dark"
+              target="_blank"
+              href="https://www.github.com/Mayanksingh2004"
+            >
+              <h5>{<GoMarkGithub />} : Mayanksingh2004</h5>
+            </a>
+            <a
+              className="text-dark"
+              target="_blank"
+              href="https://www.linkedin.com/in/mayank-singh-27a3ba181/"
+            >
+              <h5>{<GrLinkedin />} : Mayanksingh2004</h5>
+            </a>
+            <div className="margin">
+              <h4>Technologies Used : </h4>
+              <ul className="skill">
+                <li>Html</li>
+                <li>CSS</li>
+                <li>Node js</li>
+                <li>Express js</li>
+                <li>React js</li>
+                <li>MongoDB</li>
+              </ul>
+            </div>
+
+            <h4>About Me :</h4>
+            <p>
+              Hi, My Name is Mayank Kumar Singh from Renukoot, Uttar Pradesh ,
+              India. I am currently pursuing B.Tech. from GLA University,
+              Mathura, India. I like creating and designing websites. This is my
+              project for Fullstack Development -2 taught by Mr. Pankaj Kapoor
+            </p>
+          </div>
         </div>
       </div>
     </div>
