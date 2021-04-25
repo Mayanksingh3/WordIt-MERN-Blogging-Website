4f84ef581f4c2e829b89e9adaa88167b180206ba
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2021-04-25 18:29:33 +0530

    Updated NotFound UI

--- my-project/src/pages/NotFound.js
+++ my-project/src/pages/NotFound.js
@@ -1,5 +1,23 @@
 import React from "react";
+import s404 from "../assets/not.jpg";
+import { Link } from "react-router-dom";
+import "../css/NotFound.css";
 
 export default function NotFound() {
-  return <div>404 NotFound</div>;
+  return (
+    <div className="conatainer-fluid not">
+      <div className="body-not">
+        <img src={s404} alt="" />
+      </div>
+      <div className="body-not">
+        <h2>404 ERROR. PAGE NOT FOUND</h2>
+      </div>
+
+      <div className="body-not">
+        <Link to="/" className="btn btn-primary">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
 }
